9803082774f545482366260c72392c1f9d5f0932
SharkyWarky <creeperplays2020@gmail.com>
2026-08-06 20:17:50 -0600

    clean up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -42,17 +42,11 @@ function updatePage() {
 }
 
 // When clicking a link, update URL hash
-document.addEventListener("click", (event) => {
-    const link = event.target.closest("a");
-
-    if (!link) return;
-
-    const href = link.getAttribute("href");
-
-    if (href && href.startsWith("#")) {
-        event.preventDefault();
-        window.location.hash = href;
-    }
+document.querySelectorAll('nav a[href^="#"], .dropdown-content a[href^="#"]').forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.location.hash = link.getAttribute("href");
+    });
 });
 
 // Run when URL changes
@@ -60,68 +54,3 @@ window.addEventListener("hashchange", updatePage);
 
 // Run on initial load
 window.addEventListener("DOMContentLoaded", updatePage);
-
-const mapVersions = {
-    nacht: ["waw", "bo1", "bo3"],
-    verruckt: ["waw", "bo1", "bo3"],
-    "shi-no-numa": ["waw", "bo1", "bo3", "vanguard"],
-    "der-riese": ["waw", "bo1"],
-    kino: ["bo1", "bo3"],
-    ascension: ["bo1", "bo3"],
-    "shangri-la": ["bo1", "bo3"],
-    moon: ["bo1", "bo3"],
-    origins: ["bo2", "bo3"],
-    "the-giant": ["bo3"],
-};
-
-const versionNames = {
-    waw: "WaW",
-    bo1: "BO1",
-    bo2: "BO2",
-    bo3: "BO3",
-    bo4: "BO4",
-    bocw: "Cold War",
-    bo6: "BO6",
-    bo7: "BO7",
-    adancedwarfare: "AW",
-    infinitewarfare: "IW",
-    ww2: "WWII",
-    vanguard: "Vanguard",
-    mw3: "MW3",
-};
-
-function updateVersions() {
-    const container = document.querySelector(".map-versions");
-
-    container.innerHTML = "";
-
-    const currentID = window.location.hash.substring(1);
-
-    // Split shangri-la-bo3 into:
-    // shangri-la
-    // bo3
-    const parts = currentID.split("-");
-
-    const version = parts.pop();
-    const map = parts.join("-");
-
-    if (!mapVersions[map]) {
-        return;
-    }
-
-    mapVersions[map].forEach((mapVersion) => {
-        const button = document.createElement("button");
-
-        button.textContent = versionNames[mapVersion];
-
-        if (mapVersion === version) {
-            button.classList.add("active");
-        }
-
-        button.onclick = () => {
-            window.location.hash = `${map}-${mapVersion}`;
-        };
-
-        container.appendChild(button);
-    });
-}
